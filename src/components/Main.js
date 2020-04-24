18791dc5d5bfb34006cc4389416a9f331757a6a6
PetejStone <petejstoney@gmail.com>
2020-04-24 12:08:35 -0700

    testing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
+import {Redirect} from 'react-router-dom';
 
 const Main = props => {
 
+    useEffect(() => {
+       
+    })
     return (
         <div>
             <h1>StockTwits</h1>
