74619e3c47eb0b5d6208428a3dde5fedecd572d3
PetejStone <petejstoney@gmail.com>
2020-04-24 12:24:39 -0700

    removed redirect

--- src/App.js
+++ src/App.js
@@ -8,11 +8,8 @@ function App() {
 
     <div className="App">
       <h1>Hello</h1>
-      <Route
-        path="/"
-        component={ () => window.location = `https://api.stocktwits.com/api/2/oauth/authorize?client_id=87ff18157d322323&response_type=code&redirect_uri=http://stocktwits-app.netlify.app&scope=read,watch_lists,publish_messages,publish_watch_lists,direct_messages,follow_users,follow_stocks` }
- 
-      />
+      
+      <Main />
   
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import {Redirect} from 'react-router-dom';
 const Main = props => {
 
     useEffect(() => {
-       
+       window.location =  `https://api.stocktwits.com/api/2/oauth/authorize?client_id=87ff18157d322323&response_type=code&redirect_uri=http://stocktwits-app.netlify.app&scope=read,watch_lists,publish_messages,publish_watch_lists,direct_messages,follow_users,follow_stocks` 
     })
     return (
         <div>
