707529f6a46d40243ca03f1336fb8a3de7b97181
PetejStone <petejstoney@gmail.com>
2020-04-27 16:08:43 -0700

    added functionality to add/remove list items

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,10 @@ const Main = props => {
 
     const [symbol, setSymbol] = useState('')
     const [tweets, setTweets] = useState('')
-
+    const [dict, setDict] = useState({})
+    const [exists, setExists] = useState(false)
+    const [error, setError] = useState(false)
+    const [symbolId, setSymbolId] = useState(0)
     useEffect(() => {
     //    axios
     //    .get(`http://localhost:4000/${symbol}`) //temp proxy server
@@ -21,8 +24,9 @@ const Main = props => {
            
     //        setTweets(...tweets, res.data)
     //    })
+       
 
-    },[setTweets])
+    },[setTweets,setSymbolId])
 
     const handleChange = (e) => {
         setSymbol(e.target.value)
@@ -33,10 +37,25 @@ const Main = props => {
         axios
        .get(`http://localhost:4000/${symbol}`) //temp proxy server
        .then(res => {
-        //    console.log(res.data)
+
            
-           setTweets([...tweets, res.data.messages])
+            
            
+           if (symbol in dict) {
+               setError(true)
+           } else {
+                dict[symbol] = symbol
+                setTweets([...tweets, res.data.messages])
+
+                setExists(false)
+                setError(false)
+           }
+           
+           
+       })
+
+       .catch(error => {
+            setError(true)
        })
     }
 
@@ -45,21 +64,46 @@ const Main = props => {
         return {__html: text};
       }
 
+    function deletion(symbol, index) {
+        console.log('dict is currently', dict)
+        delete dict[symbol]
+        console.log('dict is now', dict)
+        
+        // setSymbolId(0)
+        // setTweets(messages)
+        tweets.filter(tweet => tweet !== tweet[index])
+        console.log('tweets are npw', tweets)
+    }
+    
     return (
         <div>
-            
+            {console.log(tweets)}
             <h1>StockTwits</h1>
+            <h2 onClick={()=> deletion()}>XX</h2>
              <p>Add Symbol</p>
             <input type="text" value={symbol} onChange={handleChange} type="symbol" />
+            {exists && <p className="error">You've already added this stock symbol, please pick another one.</p>}
+            {error && <p className="error">There has been an error. It is likely the stock symbol you entered does not exist, please try a valid symbol.</p>}
             <button onClick={onSubmit}>Add Symbol</button>
-            {console.log(tweets)}
 
-            {tweets && tweets[0].map(tweet => 
+            <div className="symbolList">
+                {dict && Object.values(dict).map((value, index) =>
+                    <div  id={index && index}
+                    onClick={() => {
+                        setSymbolId(index)
+                    }}>
+                        <p onClick={()=> deletion(value, index)}>X</p>
+                        <p>{value}</p>
+                    </div>
+                    )}
+            </div>
+            
+
+            {tweets && tweets[symbolId].map(tweet => 
                 <div className="tweet">
                  <a href={`https://stocktwits.com/${tweet.user.username}`} target="_blank"><img src={tweet.user.avatar_url} /> </a>
                  <a href={`https://stocktwits.com/${tweet.user.username}`} target="_blank"> <p>{tweet.user.username}</p> </a>
-                
-                
+             
                 <p id={tweet.id} dangerouslySetInnerHTML={createMarkup(twitter.autoLink(tweet.body))}></p>
                 
                  <a href={`https://stocktwits.com/message/${tweet.id}`} target="_blank">{new Date(tweet.created_at).toUTCString()}</a>
