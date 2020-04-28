a568cd10303f14a0828c2150368795d57cf86eed
PetejStone <petejstoney@gmail.com>
2020-04-28 13:01:43 -0700

    pulled symbol object into a state of array

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,8 @@ const Main = props => {
     const [dict, setDict] = useState({})
     const [exists, setExists] = useState(false)
     const [error, setError] = useState(false)
-    const [symbolId, setSymbolId] = useState(0)
+    const [symbolId, setSymbolId] = useState('')
+    const [array,setArray] = useState('')
     useEffect(() => {
     //    axios
     //    .get(`http://localhost:4000/${symbol}`) //temp proxy server
@@ -24,9 +25,11 @@ const Main = props => {
            
     //        setTweets(...tweets, res.data)
     //    })
+        setSymbolId(0)
+        setArray(Object.values(dict))
        
 
-    },[setTweets,setSymbolId])
+    },[dict, setArray,setDict])
 
     const handleChange = (e) => {
         setSymbol(e.target.value)
@@ -43,19 +46,23 @@ const Main = props => {
            
            if (symbol in dict) {
                setError(true)
+               
            } else {
                 dict[symbol] = symbol
+                setArray(Object.values(dict))
                 setTweets([...tweets, res.data.messages])
-
                 setExists(false)
                 setError(false)
            }
-           
+
+           document.querySelector('#input').value = ''
            
        })
 
        .catch(error => {
+             
             setError(true)
+            
        })
     }
 
@@ -65,49 +72,60 @@ const Main = props => {
       }
 
     function deletion(symbol, index) {
-        console.log('dict is currently', dict)
+        console.log('dict is currently', dict, array)
         delete dict[symbol]
+        setArray(Object.values(dict))
         console.log('dict is now', dict)
+        console.log('array is', array)
+           
+           
         
-        // setSymbolId(0)
-        // setTweets(messages)
-        tweets.filter(tweet => tweet !== tweet[index])
-        console.log('tweets are npw', tweets)
     }
     
+ 
     return (
         <div>
-            {console.log(tweets)}
+            {console.log(tweets, dict,array)}
             <h1>StockTwits</h1>
-            <h2 onClick={()=> deletion()}>XX</h2>
+           
              <p>Add Symbol</p>
-            <input type="text" value={symbol} onChange={handleChange} type="symbol" />
+            <input id="input" type="text" value={symbol} onChange={handleChange} type="symbol" />
             {exists && <p className="error">You've already added this stock symbol, please pick another one.</p>}
             {error && <p className="error">There has been an error. It is likely the stock symbol you entered does not exist, please try a valid symbol.</p>}
             <button onClick={onSubmit}>Add Symbol</button>
 
             <div className="symbolList">
-                {dict && Object.values(dict).map((value, index) =>
+                {array && array.map((value, index) =>
                     <div  id={index && index}
                     onClick={() => {
                         setSymbolId(index)
                     }}>
-                        <p onClick={()=> deletion(value, index)}>X</p>
-                        <p>{value}</p>
+                        <p onClick={()=> 
+                            
+                            deletion(value, index)
+                            }>X</p>
+                            <p>{value}</p>
+                        
                     </div>
                     )}
             </div>
             
 
-            {tweets && tweets[symbolId].map(tweet => 
-                <div className="tweet">
-                 <a href={`https://stocktwits.com/${tweet.user.username}`} target="_blank"><img src={tweet.user.avatar_url} /> </a>
-                 <a href={`https://stocktwits.com/${tweet.user.username}`} target="_blank"> <p>{tweet.user.username}</p> </a>
-             
+            {tweets && tweets[symbolId].map((tweet,index) => 
+                
+            <div className="tweet" onClick={() => {
+                tweets.splice(index,1)
+                console.log(tweets)
+            }}>
+            
+                <a href={`https://stocktwits.com/${tweet.user.username}`} target="_blank"><img src={tweet.user.avatar_url} /> </a>
+                <a href={`https://stocktwits.com/${tweet.user.username}`} target="_blank"> <p>{tweet.user.username}</p> </a>
+            
                 <p id={tweet.id} dangerouslySetInnerHTML={createMarkup(twitter.autoLink(tweet.body))}></p>
                 
-                 <a href={`https://stocktwits.com/message/${tweet.id}`} target="_blank">{new Date(tweet.created_at).toUTCString()}</a>
-                </div>
+                <a href={`https://stocktwits.com/message/${tweet.id}`} target="_blank">{new Date(tweet.created_at).toUTCString()}</a>
+            </div>
+               
                 
                 
                 )}
